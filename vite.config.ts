a306c85694c192e290f078be6cc29eb6690d76e2
ack2406 <ack9222@gmail.com>
2025-02-02 11:47:39 +0100

    add deployment stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 import plainText from "vite-plugin-plain-text";
 
 export default defineConfig({
+  base: "/nbody-sim/",
   plugins: [plainText([/\.wgsl$/])],
+  build: {
+    target: "esnext",
+  },
 });
